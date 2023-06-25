7b9db68b12c855a51d22aa8e512fdd4526259976
LemonLzy <lzy291980138@163.com>
2023-06-25 17:45:15 +0800

    Update：增加默认国家，优化代码结构

--- lfinancial/financial.py
+++ lfinancial/financial.py
@@ -3,11 +3,12 @@ from lfinancial.generators.document_type import IDCodeGenerator
 
 class Financial:
 
-    def id_code(self, document_type, country):
-        return IDCodeGenerator.generate_id(document_type, country)
+    @staticmethod
+    def id_code(document_type, country=None):
+        return IDCodeGenerator().generate_id(document_type, country)
 
 
 if __name__ == '__main__':
     f = Financial()
-    print(f.id_code('SSN', 'US'))
+    print(f.id_code('SSN'))
     print(f.id_code('SSN', 'US'))
--- lfinancial/generators/document_type.py
+++ lfinancial/generators/document_type.py
@@ -1,6 +1,32 @@
 import random
 
 
+class IDCodeGenerator:
+
+    def __init__(self):
+        self.default_country = {
+            "SSN": "US",
+            "IDCard": "CN",
+            # 其他证件类型和默认国家的映射关系
+        }
+
+    def generate_id(self, document_type, country=None):
+        if country is None:
+            country = self.default_country.get(document_type)
+
+        match document_type:
+            case "SSN":
+                doc_type = SSN()
+            case "IDCard":
+                doc_type = IDCard()
+            case "Passport":
+                doc_type = Passport()
+            case _:
+                raise ValueError(f"Unsupported document type: {document_type}.")
+
+        return doc_type.generate_id(country)
+
+
 class DocumentType:
     def generate_id(self, country):
         raise NotImplementedError("Subclasses must implement generate_id method.")
@@ -8,25 +34,31 @@ class DocumentType:
 
 class SSN(DocumentType):
     def generate_id(self, country):
-        # 根据国家代码生成 SSN 的逻辑
-        if country == 'US':
-            # 生成美国的 SSN
-            return self._generate_us_ssn()
-        elif country == 'SG':
-            # 生成新加坡的 SSN
-            return self._generate_sg_ssn()
-        else:
-            raise ValueError(f"Unsupported country: {country} for SSN generation.")
+        match country:
+            case "US":
+                return self._generate_us_ssn()
+            case _:
+                raise ValueError(f"Unsupported country: {country} for SSN generation.")
+
+    @staticmethod
+    def _is_valid_ssn(ssn):
+        digits = [int(digit) for digit in ssn.replace("-", "")]
+        if len(set(digits)) == 1:
+            return False
+
+        groups = ssn.split("-")
+        for group in groups:
+            if int(group) == 0:
+                return False
+
+        return True
 
     def _generate_us_ssn(self):
         # 生成美国 SSN 的具体逻辑
-        # ...
-        pass
-
-    def _generate_sg_ssn(self):
-        # 生成新加坡 SSN 的具体逻辑
-        # ...
-        pass
+        while True:
+            ssn = f"{random.randint(1, 999):03d}-{random.randint(1, 99):02d}-{random.randint(1, 9999):04d}"
+            if self._is_valid_ssn(ssn):
+                return ssn
 
 
 class IDCard(DocumentType):
@@ -42,20 +74,4 @@ class Passport(DocumentType):
         # ...
         pass
 
-
 # 其他证件类型的子类...
-
-class IDCodeGenerator:
-
-    @staticmethod
-    def generate_id(document_type, country):
-        if document_type == 'SSN':
-            doc_type = SSN()
-        elif document_type == 'IDCard':
-            doc_type = IDCard()
-        elif document_type == 'Passport':
-            doc_type = Passport()
-        else:
-            raise ValueError(f"Unsupported document type: {document_type}.")
-
-        return doc_type.generate_id(country)

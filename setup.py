7d55b3d8dbec7fbeaca2f3c5b999085943e9458d
LemonLzy <lzy291980138@163.com>
2023-06-26 21:52:54 +0800

    Update: Update setup.py

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ setup(
     name='lfinancial',
     version='0.0.6',
     author='zaneliu',
+    description='Generate financial test data',
     long_description=README,
     author_email='lzy291980138@163.com',
-    description='Generate financial test data',
     packages=['lfinancial'],
     install_requires=[],
     classifiers=[

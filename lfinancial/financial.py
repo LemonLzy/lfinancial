1aac107bef0535e85a55b8ad1495698153249944
LemonLzy <lzy291980138@163.com>
2023-07-04 23:10:25 +0800

    Update: private variable should use double underline

--- lfinancial/financial.py
+++ lfinancial/financial.py
@@ -5,54 +5,54 @@ class Financial:
     def __init__(self):
         self._generator_factory = GeneratorFactory()
 
-    def _generate(self, generator_name, country):
+    def __generate(self, generator_name, country):
         generator = self._generator_factory.create_generator(generator_name)
         return generator.gen(generator_name, country)
 
     def ssn(self, country=None):
-        return self._generate("SSN", country)
+        return self.__generate("SSN", country)
 
     def id_card(self, country=None):
-        return self._generate("IDCard", country)
+        return self.__generate("IDCard", country)
 
     def passport(self, country=None):
-        return self._generate("Passport", country)
+        return self.__generate("Passport", country)
 
     def nric(self, country=None):
-        return self._generate("NRIC", country)
+        return self.__generate("NRIC", country)
 
     def my_number(self, country=None):
-        return self._generate("MyNumber", country)
+        return self.__generate("MyNumber", country)
 
     def first_name(self, country=None):
-        return self._generate("first_name", country)
+        return self.__generate("first_name", country)
 
     def middle_name(self, country=None):
-        return self._generate("middle_name", country)
+        return self.__generate("middle_name", country)
 
     def last_name(self, country=None):
-        return self._generate("last_name", country)
+        return self.__generate("last_name", country)
 
     def cn_name(self, country=None):
-        return self._generate("cn_name", country)
+        return self.__generate("cn_name", country)
 
     def kana_name(self, country=None):
-        return self._generate("kana_name", country)
+        return self.__generate("kana_name", country)
 
     def cellphone(self, country=None):
-        return self._generate("cellphone", country)
+        return self.__generate("cellphone", country)
 
     def area_code(self, country=None):
-        return self._generate("area_code", country)
+        return self.__generate("area_code", country)
 
     def high_risk_country(self, country=None):
-        return self._generate("high_risk", country)
+        return self.__generate("high_risk", country)
 
     def email(self, suffix=None):
-        return self._generate("email", suffix)
+        return self.__generate("email", suffix)
 
     def bank(self, country=None):
-        return self._generate("bank", country)
+        return self.__generate("bank", country)
 
 
 if __name__ == '__main__':

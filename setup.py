b98504e4bb4dab687048288cc75097779641f23b
LemonLzy <lzy291980138@163.com>
2023-07-06 17:21:04 +0800

    Update：update packages

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 excluded_packages = ["docs", "tests", "tests.*"]
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
-
 
 setup(
     name='lfinancial',
@@ -14,7 +13,7 @@ setup(
     long_description_content_type="text/markdown",
     readme="README.md",
     author_email='lzy291980138@163.com',
-    packages=['lfinancial', 'generators', 'tests'],
+    packages=find_packages(exclude=excluded_packages),
     install_requires=[],
     classifiers=[
         'Programming Language :: Python :: 3',
